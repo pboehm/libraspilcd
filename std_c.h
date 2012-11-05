aa5f4cb1fb668869eb74fe2d66fe7fd6aedb266f
Philipp Böhm <philipp-boehm@live.de>
2012-11-05 21:23:53 +0100

    Initial State

---
+++ std_c.h
@@ -0,0 +1,46 @@
+//
+// File:        std_c.h
+// Description: Basic typedefinitions for portable C-Code
+//
+// Author:      Martin Steppuhn
+// History:     01.01.2006 Initial version
+//------------------------------------------------------------------------------
+
+#ifndef STD_C_H
+#define STD_C_H
+
+/**** Includes ****************************************************************/
+
+/**** Preprocessing directives (#define) **************************************/
+
+// Boolean values
+
+#define TRUE                1
+#define FALSE               0
+
+#define true                1
+#define false               0
+
+/**** Type definitions (typedef) **********************************************/
+
+// Standard types
+
+typedef unsigned char     uint8;
+typedef unsigned short    uint16;
+typedef unsigned long     uint32;
+
+typedef unsigned long     ulong;
+
+typedef unsigned char     bool;
+
+typedef signed char       int8;
+typedef signed short      int16;
+typedef signed long       int32;
+
+/**** Global constants (extern) ***********************************************/
+
+/**** Global variables (extern) ***********************************************/
+
+/**** Global function prototypes **********************************************/
+
+#endif
